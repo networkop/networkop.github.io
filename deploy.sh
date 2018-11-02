755e71dacb04639113f2c77b8519b50c997282e3
networkop <mmkashin@gmail.com>
2018-11-02 16:19:24 +0000

    rebuilding site Fri  2 Nov 16:19:24 GMT 2018

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+set -x
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+# Clean up public
+rm -rf public/
+
+# Add master into public as git submodule
+git submodule add --force -b master git@github.com:networkop/networkop.github.io.git public
 
 # Build the project.
 hugo -t academic # if using a theme, replace with `hugo -t <YOURTHEME>`
